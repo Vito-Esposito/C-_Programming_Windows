56ca0e6add4b7aa700b0085406a9bb92461d00b3
Ho_lus <vito.esposito001@studenti.uniparthenope.it>
2025-03-31 23:58:17 +0200

    upd

--- Primi_Esercizi/Operazioni di base su matrici 2/main.cpp
+++ Primi_Esercizi/Operazioni di base su matrici 2/main.cpp
@@ -20,6 +20,102 @@ using std:: cin;
 using std:: endl;
 
 int main(){
+    int n, m;
+
+    cout<<"Inserire n righe e m colonne di una matrice: "<<endl;
+    cin>>n;
+    cin>>m;
+    int matrice[n][m];
+
+    cout<<endl<<"Riempi matrice: "<<endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<"matrice["<<i<<"]"<<"["<<j<<"]= ";
+            cin>>matrice[i][j];  
+        }
+    }
+
+    cout<<endl<<"Stampa:"<<endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<"matrice["<<i<<"]"<<"["<<j<<"]= "<<matrice[i][j]<<endl;
+        }
+    }
+
+    cout<<endl<<"Ci sono elementi nulli?";
+    int trovato= 0, cnt=0, vettore[20], k=0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(matrice[i][j] == 0){
+                trovato= 1;
+                cnt++;
+            }
+            else{
+                vettore[k]= matrice[i][j];
+                k++;
+            }
+        }
+    }
+    if(trovato == 1){
+        cout<<endl<<"Elementi nulli trovati e ne sono "<<cnt<<".";
+        cout<<endl<<"Vettore con gli elementi non nulli della matrice: "<<endl;
+        for(int i=0; i<k; i++)
+            cout<<"vettore["<<i<<"]= "<<vettore[i]<<endl;
+    }
+    else
+        cout<<endl<<"Elementi nulli non trovati";
+
+    int n_c=n, m_c=m;
+    int matrice_copia[n_c][m_c];
+    char risposta;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            matrice_copia[i][j]= matrice[i][j];
+        }
+    }
+    cout<<endl<<"Eliminare riga (r) o colonna (c) della matrice copia? ";
+    int r_c;
+    cin>>risposta;
+    while(risposta!='c' && risposta!='r'){
+        cout<<"Riprova"<<endl;
+        cin>>risposta;
+    }
+    if(risposta == 'r'){
+        cout<<"Inserisci indice riga: "<<endl;
+        cin>>r_c;
+
+        for(int i=r_c; i<n_c; i++){
+            for(int j=0; j<m_c; j++){
+                matrice_copia[i][j]= matrice_copia[i+1][j];
+            }
+        }
+        n_c= n-1;
+        cout<<endl<<"Stampa:"<<endl;
+        for(int i=0; i<n_c; i++){
+            for(int j=0; j<m_c; j++){
+                cout<<"matrice_copia["<<i<<"]"<<"["<<j<<"]= "<<matrice_copia[i][j]<<endl;
+            }
+        }
+    }
+    else if(risposta == 'c'){
+        cout<<endl<<"Inserisci indice colonna: "<<endl;
+        cin>>r_c;
+
+        for(int i=0; i<n_c; i++){
+            for(int j=r_c; j<m_c; j++){
+                matrice_copia[i][j]= matrice_copia[i][j+1];
+            }
+        }
+        m_c= m-1;
+        cout<<endl<<"Stampa:"<<endl;
+        for(int i=0; i<n_c; i++){
+            for(int j=0; j<m_c; j++){
+                cout<<"matrice_copia["<<i<<"]"<<"["<<j<<"]= "<<matrice_copia[i][j]<<endl;
+            }
+        }
+    }
+    
+
 
 
 
