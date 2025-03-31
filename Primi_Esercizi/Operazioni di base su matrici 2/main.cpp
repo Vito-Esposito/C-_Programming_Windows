1f0306af41790be42343f46ac7a318b45a75b372
Ho_lus <vito.esposito001@studenti.uniparthenope.it>
2025-03-31 22:37:14 +0200

    up

---
+++ Primi_Esercizi/Operazioni di base su matrici 2/main.cpp
@@ -0,0 +1,27 @@
+/*
+f) Verificare se esistono elementi nulli nella matrice e contemporaneamente
+memorizzare gli elementi non nulli in un vettore V. Al termine stampare il numero
+degli elementi nulli e il vettore V.
+g) Dopo aver fatto la copia della matrice M, richiedere la posizione di una riga
+(colonna) da eliminare dalla matrice copia (M_copia).
+h) Calcolare la matrice trasposta
+i) Verificare se la matrice è simmetrica
+j) Copiare nei vettori VP e VS rispettivamente gli elementi della diagonale principale
+e secondaria.
+k) Memorizzare nei vettori MR e MC rispettivamente i minimi di ciascuna riga e di
+ciascuna colonna.
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using std:: cout;
+using std:: cin;
+using std:: endl;
+
+int main(){
+
+
+
+
+}
